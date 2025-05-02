b056d92e78d720503a5f8a87041b32a3ea5ab221
Alejandro Tapia <78757063+AlejandroSTapia@users.noreply.github.com>
2025-05-02 16:35:13 -0600

    fix url

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,9 @@
 const config = {
-    apiUrlBase: "https://portafolioaspwebapi-ascve4g0ezchgqf5.mexicocentral-01.azurewebsites.net",
+    apiUrlBase: "https://portafolioaspwebapi-ascve4g0ezchgqf5.mexicocentral-01.azurewebsites.net/",
     apiUrlBaseDev: "https://localhost:7037/" // URL para desarrollo
 };
 
 export default config;
+
+https://portafolioaspwebapi-ascve4g0ezchgqf5.mexicocentral-01.azurewebsites.net/
+https://portafolioaspwebapi-ascve4g0ezchgqf5.mexicocentral-01.azurewebsites.netapi/Resource/GetResources
