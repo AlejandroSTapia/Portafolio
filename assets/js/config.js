8622e329e2475399cdf42038d367eb084760a7ff
Alejandro Tapia <78757063+AlejandroSTapia@users.noreply.github.com>
2025-05-02 16:37:56 -0600

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,6 +4,3 @@ const config = {
 };
 
 export default config;
-
-https://portafolioaspwebapi-ascve4g0ezchgqf5.mexicocentral-01.azurewebsites.net/
-https://portafolioaspwebapi-ascve4g0ezchgqf5.mexicocentral-01.azurewebsites.netapi/Resource/GetResources
