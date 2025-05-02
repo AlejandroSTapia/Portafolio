eb1e241d520bcdfe7dd7382fcd6e5ea14311fa91
Alejandro Tapia <78757063+AlejandroSTapia@users.noreply.github.com>
2025-05-02 13:08:57 -0600

    Change urlapi
    Se actualizo la url de la api de portaflio en azure

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiUrlBase: "https://portafoliowebapi-a5g0azgkgzavhacc.mexicocentral-01.azurewebsites.net/",
+    apiUrlBase: "https://portafolioaspwebapi-ascve4g0ezchgqf5.mexicocentral-01.azurewebsites.net",
     apiUrlBaseDev: "https://localhost:7037/" // URL para desarrollo
 };
 
